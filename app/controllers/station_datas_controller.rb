0d32a2fd797035c24ed07e30c3a701e5ee88195e
luping <165340815@qq.com>
2014-05-05 17:06:13 +0800

    luping0405

--- app/controllers/station_datas_controller.rb
+++ app/controllers/station_datas_controller.rb
@@ -22,19 +22,109 @@ class StationDatasController < ApplicationController  #系统设置-工位
   def edit
     @station = Station.find_by_id(params[:id].to_i)
     @station_services = @station.products.map(&:id)
-    @categories = Category.where(["types = ? and store_id = ? ", Category::TYPES[:service], @store.id]).inject({}){|hash,c|hash[c.id]=c.name;hash};
-    @services = @categories.empty? ? {}:Product.is_normal.where(:category_id => @categories.keys).group_by { |p| p.category_id }
+    @category = Category.where(["types = ? and store_id = ? ", Category::TYPES[:service], @store.id]).inject({}){|hash,c|hash[c.id]=c.name;hash};
+    @services = @category.empty? ? {}:Product.is_normal.where(:category_id => @category.keys).group_by { |p| p.category_id }
     pack_serv = Product.is_normal.where(:category_id => Product::PACK[:PACK],:store_id=>@store.id)
     unless pack_serv.blank?
-      @categories.merge!(Product::PACK_SERVIE)
+      @category.merge!(Product::PACK_SERVIE)
       @services.merge!(Product::PACK[:PACK]=>pack_serv)
     end
-    
     respond_to do |f|
       f.js
     end
   end
-  
+
+  def update
+    name = params[:station_name]
+    code = params[:station_code]
+    has_controller = params[:station_has_controller].nil? || params[:station_has_controller].to_i==0 ? false : true
+    coll_code = params[:station_collector_code]
+    servs = params[:station_servs]
+    Station.transaction do
+      @station = Station.find_by_id(params[:id].to_i)
+      begin
+        @station.update_attributes({:name => name, :code => code, :is_has_controller => has_controller,
+            :collector_code => coll_code})
+        StationServiceRelation.delete_all(["station_id=?", @station.id])
+        servs.each do |s|
+          StationServiceRelation.create(:station_id => @station.id, :product_id => s.to_i)
+        end if servs
+        @has_services = @station.products.map(&:name)
+        @status = 1
+      rescue
+        @status = 0
+      end
+    end
+  end
+
+  def new
+    @category = Category.where(["types = ? and store_id = ? ", Category::TYPES[:service], @store.id]).inject({}){|hash,c|hash[c.id]=c.name;hash};
+    @services = @category.empty? ? {}:Product.is_normal.where(:category_id => @category.keys).group_by { |p| p.category_id }
+    pack_serv = Product.is_normal.where(:category_id => Product::PACK[:PACK],:store_id=>@store.id)
+    unless pack_serv.blank?
+      @category.merge!(Product::PACK_SERVIE)
+      @services.merge!(Product::PACK[:PACK]=>pack_serv)
+    end
+    respond_to do |f|
+      f.js
+    end
+  end
+
+  def create
+    name = params[:station_name]
+    code = params[:station_code]
+    has_controller = params[:station_has_controller].nil? || params[:station_has_controller].to_i==0 ? false : true
+    coll_code = params[:station_collector_code]
+    servs = params[:station_servs]
+    Station.transaction do
+      begin
+        station = Station.create({:store_id => @store.id, :name => name, :code => code, :is_has_controller => has_controller,
+            :collector_code => coll_code, :status => Station::STAT[:NORMAL]})
+        servs.each do |s|
+          StationServiceRelation.create(:station_id => station.id, :product_id => s.to_i)
+        end if servs
+        flash[:notice] = "创建成功!"
+      rescue
+        flash[:notice] = "创建失败!"
+      end
+    end
+    redirect_to store_station_datas_path(@store)
+  end
+
+  def destroy
+    Station.transaction do
+      station = Station.find_by_id(params[:id].to_i)
+      if station.update_column(:status, Station::STAT[:DELETED])
+        flash[:notice] = "删除成功!"
+      else
+        flash[:notice] = "删除失败!"
+      end
+      redirect_to store_station_datas_path(@store)
+    end
+  end
+
+  def name_valid
+    type = params[:type].to_i
+    name = params[:name]
+    code = params[:code]
+    status = 1
+    if type == 0  #新建时验证
+      obj = Station.where(["status!=? and store_id=? and (name=? or code=?)", Station::STAT[:DELETED], @store.id,
+          name, code]).first
+      if obj
+        status = 0
+      end
+    elsif type == 1 #编辑时验证
+      s_id = params[:station_id].to_i
+      obj = Station.where(["id!=? and status!=? and store_id=? and (name=? or code=?)", s_id, Station::STAT[:DELETED],
+          @store.id, name, code]).first
+      if obj
+        status = 0
+      end
+    end
+    render :json => {:status => status}
+  end
+
   def get_title
     @title = "工位设置"
   end
--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,9 +1,9 @@
 #encoding: utf-8
-class WelcomesController < ApplicationController
+class WelcomesController < ApplicationController  #主页
   before_filter :has_sign?
   layout :false
   def index
-    
+    @title = "欢迎"
   end
   
 end
